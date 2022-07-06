024710b66511a704794fad9fc72b5ac3668b148c
Kono Tyran <HAklowner@gmail.com>
2022-07-05 17:14:43 -0700

    remove workaround for bug in satchel that set defaults to 10.

--- APMapMod/UI/BetterMenu.cs
+++ APMapMod/UI/BetterMenu.cs
@@ -1,4 +1,3 @@
-using APMapMod.Util;
 using Modding;
 using Satchel;
 using Satchel.BetterMenus;
@@ -11,15 +10,13 @@ namespace APMapMod.UI;
 
 internal static class BetterMenu
 {
-    private static Menu _menuRef;
-    private static Image _sr;
-    private static bool _update;
+    private static Menu menuRef;
+    private static Image sr;
 
     public static MenuScreen GetMenuScreen(MenuScreen modListMenu, ModToggleDelegates? toggleDelegates)
     {
-        _menuRef ??= PrepareMenu();
-        _menuRef.OnBuilt += BuildUpdate;
-        return _menuRef.GetMenuScreen(modListMenu);
+        menuRef ??= PrepareMenu();
+        return menuRef.GetMenuScreen(modListMenu);
     }
 
     private static Menu PrepareMenu()
@@ -29,63 +26,51 @@ internal static class BetterMenu
             new TextPanel("Enter the Red Green and Blue values for your icon color", 800f),
             new MenuButton("Random", "", _ => {
                 APMapMod.GS.IconColor = Utils.GetRandomLightColor();
-                _sr.color = APMapMod.GS.IconColor;
-                _menuRef.Update();
+                sr.color = APMapMod.GS.IconColor;
+                menuRef.Update();
             }),
             new CustomSlider(
                 "Red",
                 r =>
                 {
-                    APMapMod.GS.IconColorR = _update ? Mathf.RoundToInt(r) : APMapMod.GS.IconColorR;
-                    _sr.color = APMapMod.GS.IconColor;
+                    APMapMod.GS.IconColorR = Mathf.RoundToInt(r);
+                    sr.color = APMapMod.GS.IconColor;
                 },
-                () => APMapMod.GS.IconColorR
-            )
-            {
-                minValue = 0,
-                maxValue = 255,
-                wholeNumbers = true
-            },
+                () => APMapMod.GS.IconColorR,
+                0,
+                255,
+                true
+                ),
             new CustomSlider(
                 "Green",
                 g =>
                 {
-                    APMapMod.GS.IconColorG = _update ? Mathf.RoundToInt(g) : APMapMod.GS.IconColorG;
-                    _sr.color = APMapMod.GS.IconColor;
+                    APMapMod.GS.IconColorG = Mathf.RoundToInt(g);
+                    sr.color = APMapMod.GS.IconColor;
                 },
-                () => APMapMod.GS.IconColorG
-            )
-            {
-                minValue = 0,
-                maxValue = 255,
-                wholeNumbers = true
-            },
+                () => APMapMod.GS.IconColorG,
+                0,
+                255,
+                true
+            ),
             new CustomSlider(
                 "Blue",
                 b =>
                 {
-                    APMapMod.GS.IconColorB = _update ? Mathf.RoundToInt(b) : APMapMod.GS.IconColorB;
-                    _sr.color = APMapMod.GS.IconColor;
+                    APMapMod.GS.IconColorB = Mathf.RoundToInt(b);
+                    sr.color = APMapMod.GS.IconColor;
                 },
-                () => APMapMod.GS.IconColorB
-            )
-            {
-                minValue = 0,
-                maxValue = 255,
-                wholeNumbers = true
-            },
+                () => APMapMod.GS.IconColorB,
+                0,
+                255,
+                true
+            ),
             new StaticPanel(
                 "preview icon",
                 CreateIcon,
                 100f
             ),
-    });
-    }
-
-    private static void BuildUpdate(object sender, ContainerBuiltEventArgs containerBuiltEventArgs)
-    {
-        _menuRef.Update();
-        _update = true;
+        });
     }
 
     private static void CreateIcon(GameObject go)
@@ -101,9 +86,9 @@ internal static class BetterMenu
         var tex = GUIController.Instance.Images["CompassIcon"];
         var compassIcon = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 55);
         knightIcon.AddComponent<CanvasRenderer>();
-        _sr = knightIcon.AddComponent<Image>();
-        _sr.sprite = compassIcon;
-        _sr.color = APMapMod.GS.IconColor;
+        sr = knightIcon.AddComponent<Image>();
+        sr.sprite = compassIcon;
+        sr.color = APMapMod.GS.IconColor;
         knightIcon.transform.localPosition = new Vector3(0, -tex.height/2f, 0);
         knightIcon.layer = 27; // uGUI layer
         knightIcon.SetScale(1,1);
